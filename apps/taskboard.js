c98dc611b1e9198e87473d2727f17504811c7d5d
mnemonicpunk <jmbendig@gmail.com>
2025-01-02 20:31:58 +0100

    fix: handle maxed out taskboard delivery slots

--- apps/taskboard.js
+++ apps/taskboard.js
@@ -97,11 +97,14 @@ export default class TaskboardApp extends NerdHudApp {
         let have_storage = this.importAppFunction('storage.have');
         let highlight_storage = this.importAppFunction('storage.highlight');
         let price = this.importAppFunction('market.price');
-
-        let in_inventory = have_inventory(order.requestItems[0].itemId)
-        let in_storage = have_storage(order.requestItems[0].itemId);
-        let have = in_inventory+in_storage;
-
+    
+        // Handle the edge case where requestItems[0] might not exist
+        let request_items = order.requestItems && order.requestItems[0] ? order.requestItems[0] : { itemId: '', quantity: 0 };
+    
+        let in_inventory = have_inventory(request_items.itemId);
+        let in_storage = have_storage(request_items.itemId);
+        let have = in_inventory + in_storage;
+    
         let order_slot = this._order_cache[slot];
         if (!order_slot) {
             let elements = {
@@ -113,31 +116,31 @@ export default class TaskboardApp extends NerdHudApp {
                 details_reward: document.createElement('div'),
                 details_cost: document.createElement('div'),
                 countdown: document.createElement('div')
-            }
-
+            };
+    
             elements.main.addEventListener('click', () => {
                 let order = this.orders[slot];
                 let market_entry = document.getElementsByClassName('Marketplace_filter__3ynr2');
                 if (market_entry[0]) {
                     const element = market_entry[0];
-                    const text = this.sys.getItemName(order.requestItems[0].itemId);
-
+                    const text = this.sys.getItemName(request_items.itemId);
+    
                     // Set the value programmatically
                     Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, 'value').set.call(element, text);
-
+    
                     // Create and dispatch an input event to simulate React's onChange handling
                     const inputEvent = new Event('input', { bubbles: true });
                     element.dispatchEvent(inputEvent);
                 } else {
                     let show_item = this.importAppFunction('iteminfo.show_item');
-                    show_item(order.requestItems[0].itemId);
+                    show_item(request_items.itemId);
                 }
             });
-
+    
             let img_holder = document.createElement('div');
             img_holder.appendChild(elements.details_img);
-            img_holder.style = "display: flex; align-items: center; margin-right: 11px;"
-
+            img_holder.style = "display: flex; align-items: center; margin-right: 11px;";
+    
             elements.main.appendChild(elements.order);
             elements.order.appendChild(img_holder);
             elements.order.appendChild(elements.details);
@@ -145,43 +148,41 @@ export default class TaskboardApp extends NerdHudApp {
             elements.details.appendChild(elements.details_reward);
             elements.details.appendChild(elements.details_cost);
             elements.main.appendChild(elements.countdown);
-
+    
             elements.main.className = "hud_interactive_list";
-
+    
             // apply formatting
             elements.order.style.padding = "6px";
             elements.order.className = "hud_interactive_list_item";
-            //elements.order.style.verticalAlign = "middle";
-
+    
             let img = elements.details_img;
-            img.className="hud_icon_large";
-            //img.src = this.sys.getItemData(order.requestItems[0].itemId).image;
+            img.className = "hud_icon_large";
             img.style.marginRight = "6px";
             let countdown = elements.countdown;
-
+    
             countdown.style.textAlign = "center";
             countdown.style.color = '#aaa';
             countdown.style.padding = "11px";
             countdown.style.paddingBottom = "11px";
-
+    
             this.window.appendChild(elements.main);
-            
+    
             order_slot = this._order_cache[slot] = elements;
-
+    
             elements.details.addEventListener('mouseover', () => {
-                highlight_storage(elements.details.dataset.highlight_itm)
+                highlight_storage(elements.details.dataset.highlight_itm);
             });
             elements.details.addEventListener('mouseout', () => {
-                highlight_storage()
+                highlight_storage();
             });
         }
-        
+    
         // update where necessary
-        let img_src = this.sys.getItemData(order.requestItems[0].itemId).image;
-        let title_text = have + "/" + order.requestItems[0].quantity + "&nbsp;" + this.sys.getItemName(order.requestItems[0].itemId);
-        let reward_text  = '<img class="hud_icon_small" src="' + this.sys.getCurrencyData(order.reward.currency.currencyId).sprite.image +'">&nbsp;' + order.reward.currency.amount + '&nbsp;&nbsp;&nbsp;<img class="hud_icon_small" src="https://d31ss916pli4td.cloudfront.net/game/ui/skills/skills_icon_' + order.reward.skill.skillType + '.png?v6">&nbsp;' + order.reward.skill.xp;
+        let img_src = this.sys.getItemData(request_items.itemId).image;
+        let title_text = have + "/" + request_items.quantity + "&nbsp;" + this.sys.getItemName(request_items.itemId);
+        let reward_text = '<img class="hud_icon_small" src="' + this.sys.getCurrencyData(order.reward?.currency?.currencyId || "cur_coins").sprite.image + '">&nbsp;' + order.reward.currency.amount + '&nbsp;&nbsp;&nbsp;<img class="hud_icon_small" src="https://d31ss916pli4td.cloudfront.net/game/ui/skills/skills_icon_' + order.reward.skill.skillType + '.png?v6">&nbsp;' + order.reward.skill.xp;
         let countdown_text = "";
-
+    
         order_slot.details_title.style.background = "";
         if (order.playerClassId == "vip") {
             order_slot.details_title.style.background = "#007";
@@ -189,32 +190,31 @@ export default class TaskboardApp extends NerdHudApp {
         if (order.playerClassId == "landowner") {
             order_slot.details_title.style.background = "#666600";
         }
-
+    
         if (order_slot.details_img.src != img_src) {
             order_slot.details_img.src = img_src;
         }
         if (order_slot.details_title.innerHTML != title_text) {
-            order_slot.details.dataset.highlight_itm =  order.requestItems[0].itemId;
+            order_slot.details.dataset.highlight_itm = request_items.itemId;
             order_slot.details_title.innerHTML = title_text;
         }
-        
+    
         if (order_slot.details_reward.innerHTML != reward_text) {
             order_slot.details_reward.innerHTML = reward_text;
-            
         }
-
+    
         let title_color = '';
-        let cost_text = 'Buy ' + (order.requestItems[0].quantity - have) + ' for <img class="hud_icon_small" src="' + this.sys.getCurrencyData('cur_coins').sprite.image +'">&nbsp;' + this.sys.formatCurrency(price(order.requestItems[0].itemId) * (order.requestItems[0].quantity - have));
-        if (price(order.requestItems[0].itemId) == 0) {
-            cost_text = "Craft " + (order.requestItems[0].quantity - have) + " more!";
+        let cost_text = 'Buy ' + (request_items.quantity - have) + ' for <img class="hud_icon_small" src="' + this.sys.getCurrencyData('cur_coins').sprite.image + '">&nbsp;' + this.sys.formatCurrency(price(request_items.itemId) * (request_items.quantity - have));
+        if (price(request_items.itemId) == 0) {
+            cost_text = "Craft " + (request_items.quantity - have) + " more!";
         }
-
+    
         order_slot.details_title.style.fontWeight = "normal";
-        if (have >= order.requestItems[0].quantity) {
+        if (have >= request_items.quantity) {
             title_color = '#F0E68C';
-            cost_text = "Get " + (order.requestItems[0].quantity - in_inventory) + " from storage!";
+            cost_text = "Get " + (request_items.quantity - in_inventory) + " from storage!";
         }
-        if (in_inventory >= order.requestItems[0].quantity) {
+        if (in_inventory >= request_items.quantity) {
             order_slot.details_title.style.fontWeight = "900";
             title_color = '#17B169';
             cost_text = "Ready to deliver!";
@@ -222,20 +222,24 @@ export default class TaskboardApp extends NerdHudApp {
         if (order_slot.details_cost.innerHTML != cost_text) {
             order_slot.details_cost.innerHTML = cost_text;
         }
-
+    
         if (order_slot.details_title.style.color != title_color) {
             order_slot.details_title.style.color = title_color;
         }
-
+    
         // Switch between order and countdown display if necessary
         if (order.deliveredAt) {
             let finish_time = order.deliveredAt + order.cooldown;
             if (finish_time > Date.now()) {
-                countdown_text = "New order in " + this.sys.formatRelativeTime(finish_time);
+                if (request_items.itemId == '') {
+                    countdown_text = "Order slot maxed out!";
+                } else {
+                    countdown_text = "New order in " + this.sys.formatRelativeTime(finish_time);
+                }
             } else {
-                countdown_text = "New order ready!"
+                countdown_text = "New order ready!";
             }
-
+    
             if (order_slot.countdown.innerHTML != countdown_text) {
                 order_slot.countdown.innerHTML = countdown_text;
             }
@@ -243,14 +247,13 @@ export default class TaskboardApp extends NerdHudApp {
                 order_slot.countdown.style.display = "block";
                 order_slot.order.style.display = "none";
             }
-            
         } else {
             if (order_slot.order.style.display != "flex") {
                 order_slot.order.style.display = "flex";
                 order_slot.countdown.style.display = "none";
             }
-        }        
-    }
+        }
+    }    
     getRequested(item) {
         let amount = 0;
         for (let i=0; i<this.orders.length; i++) {
