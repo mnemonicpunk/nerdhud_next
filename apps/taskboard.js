2517ba9d496849374d85f02587ffea2911237d3d
mnemonicpunk <jmbendig@gmail.com>
2025-01-08 03:26:08 +0100

    fix: prevent taskboard from no longer loading when orders are maxed out for a skill

--- apps/taskboard.js
+++ apps/taskboard.js
@@ -186,9 +186,9 @@ export default class TaskboardApp extends NerdHudApp {
         }
     
         // update where necessary
-        let img_src = this.sys.getItemData(request_items.itemId).image;
+        let img_src = this.sys.getItemData(request_items.itemId)?.image || "#";
         let title_text = have + "/" + request_items.quantity + "&nbsp;" + this.sys.getItemName(request_items.itemId);
-        let reward_text = '<img class="hud_icon_small" src="' + this.sys.getCurrencyData(order.reward?.currency?.currencyId || "cur_coins").sprite.image + '">&nbsp;' + order.reward.currency.amount + '&nbsp;&nbsp;&nbsp;<img class="hud_icon_small" src="https://d31ss916pli4td.cloudfront.net/game/ui/skills/skills_icon_' + order.reward.skill.skillType + '.png?v6">&nbsp;' + order.reward.skill.xp;
+        let reward_text = '<img class="hud_icon_small" src="' + this.sys.getCurrencyData(order.reward?.currency?.currencyId || "cur_coins").sprite.image + '">&nbsp;' + order.reward?.currency?.amount + '&nbsp;&nbsp;&nbsp;<img class="hud_icon_small" src="https://d31ss916pli4td.cloudfront.net/game/ui/skills/skills_icon_' + order.reward?.skill?.skillType + '.png?v6">&nbsp;' + order.reward?.skill?.xp;
         let countdown_text = "";
     
         order_slot.details_title.style.background = "";
