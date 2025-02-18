1b72d25c9a650362e8018248aef5a0f2a8051d42
mnemonicpunk <jmbendig@gmail.com>
2025-02-18 20:47:57 +0100

    fix: make creator code actually show up

--- apps/notification.js
+++ apps/notification.js
@@ -19,10 +19,20 @@ export default class NotificationApp extends NerdHudApp {
             });
         });
 
-        this.queueNotification("Want to support Nerd HUD Next?", "Use creator code ILIKEMONEY to get 5% off Pixel purchases!")
+        
     }
     event(type, data) {
         super.event(type, data);
+        if (type == "enter_game") {
+            window.setTimeout(() => {
+                
+                this.queueNotification({
+                    title: "Want to support Nerd HUD Next?", 
+                    message: "Use creator code ILIKEMONEY to get 5% off Pixel purchases!"
+                });
+
+            }, 5000)
+        }
         if (type == "timer_group_finished") {
 
             let t = data[0];
